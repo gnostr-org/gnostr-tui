29d94f260bb4a5cd5f2afa31db164adf1dc3e5b9
@RandyMcMillan <randy.lee.mcmillan@gmail.com>
2023-12-18 11:25:45 -0500

    cargo.mk:help

--- cargo.mk
+++ cargo.mk
@@ -1,32 +1,38 @@
-##
-##make cargo-*
+## CARGO
 cargo-help:### 	cargo-help
 	@awk 'BEGIN {FS = ":.*?###"} /^[a-zA-Z_-]+:.*?###/ {printf "\033[36m%-15s\033[0m %s\n", $$1, $$2}' $(MAKEFILE_LIST)
 
 cargo-install:### 	cargo install --path .
-#
-	#@$(CARGO) install --path $(PWD)
+## cargo install --locked --path `pwd`
 	@$(CARGO) install --locked --path $(PWD)
 
 cargo-i-gnostr-legit:cargo-install-gnostr-legit### 	cargo-i-gnostr-legit
 cargo-install-gnostr-legit:
+## cargo install --bins --path ./legit
 	@$(CARGO) install --bins $(QUIET) --path ./legit
+
 cargo-bench:### 	cargo-bench
-## $(CARGO) bench
+## cargo bench
 	@$(CARGO) bench
 
 cargo-examples:### 	cargo-examples
-## $(CARGO) b --examples
+## cargo b --examples
 	@$(CARGO) b --examples
 
 cargo-report:### 	cargo-report
+## cargo report future-incompatibilities --id 1
 	$(CARGO) report future-incompatibilities --id 1
 
 cargo-doc:### 	cargo-doc
+## cargo doc
+## cargo doc --no-deps
+## cargo doc --no-deps --open
 	 $(CARGO) doc #--no-deps #--open
 
 cargo-nightly-udeps:### 	cargo-nightly-udeps
+## cargo +nightly udeps
 	 $(CARGO) +nightly udeps
 
+## :
 # vim: set noexpandtab:
 # vim: set setfiletype make
